e6eebdea604e9110dd278a15a6abb8d628b34782
Alessandro Vozza <alessandro.vozza@microsoft.com>
2025-11-06 19:39:25 +0100

    Add Umami analytics script to onRenderBody for tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,11 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: 'en', prefix: 'og: http://ogp.me/ns#' });
+  setHeadComponents([
+    <script
+      key="umami-analytics"
+      defer
+      src="https://cloud.umami.is/script.js"
+      data-website-id="fdac0556-9492-4777-9cb3-1b22b8220aba"
+    />,
+  ]);
 };
