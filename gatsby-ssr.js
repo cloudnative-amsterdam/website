c870c7d234d5fb1691ecf0d41563f84d8bd59317
Alessandro Vozza <alessandro.vozza@microsoft.com>
2025-11-06 19:51:26 +0100

    Refactor Umami analytics script to use React.createElement for consistency

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,13 @@
+const React = require('react');
+
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: 'en', prefix: 'og: http://ogp.me/ns#' });
   setHeadComponents([
-    <script
-      key="umami-analytics"
-      defer
-      src="https://cloud.umami.is/script.js"
-      data-website-id="fdac0556-9492-4777-9cb3-1b22b8220aba"
-    />,
+    React.createElement('script', {
+      key: 'umami-analytics',
+      defer: true,
+      src: 'https://cloud.umami.is/script.js',
+      'data-website-id': 'fdac0556-9492-4777-9cb3-1b22b8220aba',
+    }),
   ]);
 };
